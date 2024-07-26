aaad9f449b88b2eaad9d805fd6eb57feecb254d7
pushkar-gr <gr.pushkar.7@gmail.com>
2024-07-26 12:54:02 +0000

    removed un nessesary function

--- src/lb/algos/weighted_round_robin.rs
+++ src/lb/algos/weighted_round_robin.rs
@@ -23,8 +23,10 @@ impl WeightedRoundRobin {
             Self::gcd(b, a % b)
         }
     }
+}
 
-    fn get_next_server(&self, config: &MutexGuard<Config>) -> Option<usize> {
+impl LoadBalancer for WeightedRoundRobin {
+    fn get_index(&mut self, config: Arc<&MutexGuard<Config>>) -> Option<usize> {
         let weights: Vec<usize> = config.servers.iter().map(|s| s.weight as usize).collect();
         let max_weight = *weights.iter().max().unwrap_or(&1);
         let gcd_weight = weights.iter().copied().reduce(Self::gcd).unwrap_or(1);
@@ -49,11 +51,5 @@ impl WeightedRoundRobin {
                 return Some(index);
             }
         }
-    }
-}
-
-impl LoadBalancer for WeightedRoundRobin {
-    fn get_index(&mut self, config: Arc<&MutexGuard<Config>>) -> Option<usize> {
-        self.get_next_server(&config)
     }
 }
